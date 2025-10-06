cbd4d60a558e65c527ed02521ef467edaa47b68b
AminAttaei <aminattaei2000@gmail.com>
2025-10-06 14:06:55 +0330

    Updated dnn codes

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
+import numpy as np
+from imutils import face_utils
 import cv2
-
-net = cv2.dnn.readNetFromCaffe('./models/deploy.prototxt.txt','./models/res10_300x300_ssd_iter_140000_fp16.caffemodel')
-
-image = cv2.imread('./images/big_team.jpg')
-h,w = image.shape[:2]
-
-blob = cv2.dnn.blobFromImage(cv2.resize(image,(300,300)),1,(300,300),(104,177,123),False)
-
-net.setInput(blob)
-
-detections = net.forward()
-
-for i in range(0,detections.shape[2]):
-    confidence = detections[0,0,i,2]
-
-    if confidence > 0.5:
-        box = detections[0,0,i,3:7] * [w,h,w,h]
-        (x1,y1,x2,y2) = box.astype("int")
-        cv2.rectangle(image,(x1,y1),(x2,y2),(0,255,255),2)
+import dlib
 
 
-cv2.imshow("Faces", image)
+# Shape Detector
+shape_detector = dlib.shape_predictor('./models/shape_predictor_68_face_landmarks.dat')
+
+# face descriptor
+shape_descriptor = dlib.face_recognition_model_v1('./models/dlib_face_recognition_resnet_model_v1.dat')
+
+img = cv2.imread('./images/face.jpg')
+
+image = img.copy()
+
+face_detector = dlib.get_frontal_face_detector()
+faces = face_detector(image)
+
+for box in faces:
+    pt1 = box.left(),box.top()
+    pt2 = box.right(),box.bottom()
+
+    face_shape = shape_detector(image,box)
+    face_shape_array = face_utils.shape_to_np(face_shape,"int")
+    # face_descriptor = shape_detector.compute_face_descriptor(image,face_shape)
+
+    for point in face_shape_array:
+        cv2.circle(image,tuple(point),3,(0,255,0),-1)
+        cv2.rectangle(image,pt1,pt2,(0,255,0),3)
+
+cv2.imshow('Faces', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
